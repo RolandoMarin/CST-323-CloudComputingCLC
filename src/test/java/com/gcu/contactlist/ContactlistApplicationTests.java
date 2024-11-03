9359c83f314a0c90ddbc9dd8a311f0f9da635ebb
RolandoMarin <RolandoGalvanM@outlook.com>
2024-11-03 12:45:33 -0800

    First Commit

---
+++ src/test/java/com/gcu/contactlist/ContactlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcu.contactlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
