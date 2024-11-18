24ea1fe0fcf41f23591ef560d48d93bf69ae10ca
Chropankers <chropankers@gmail.com>
2024-11-18 01:00:01 -0500

    Commit again

--- src/main/java/com/gcu/ContactlistApplication.java
+++ src/main/java/com/gcu/ContactlistApplication.java
@@ -1,13 +1,18 @@
 package com.gcu;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ContactlistApplication {
+    private static final Logger logger = LoggerFactory.getLogger(ContactlistApplication.class);
 
 	public static void main(String[] args) {
+        logger.info("Entering method main");
 		SpringApplication.run(ContactlistApplication.class, args);
+        logger.info("Exiting method main");
 	}
 
 }
